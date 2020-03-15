2c3d39fcbdb4925760c2a5642505143a9ee55bf3
Sergey Borisov <it.saborisov@gmail.com>
2020-03-15 16:27:34 +0300

    addBorder

--- NavigationBar/ViewController.swift
+++ NavigationBar/ViewController.swift
@@ -1,15 +1,24 @@
+//
 //  ViewController.swift
 //  NavigationBar
+//
 //  Created by Сергей Борисов on 14.03.2020.
 //  Copyright © 2020 Сергей Борисов. All rights reserved.
+//
 
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var avatarImage: UIImageView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        avatarImage.layer.borderWidth = 3.0
+        avatarImage.layer.borderColor = UIColor.lightGray.cgColor
 
     }
 
+
+}
 
 
