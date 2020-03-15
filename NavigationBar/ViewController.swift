c8342134977f3c4b7cdc013f49ee23aa0f916fc1
Sergey Borisov <it.saborisov@gmail.com>
2020-03-15 17:07:36 +0300

    deleteComment

--- NavigationBar/ViewController.swift
+++ NavigationBar/ViewController.swift
@@ -15,15 +15,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         avatarImage.layer.borderWidth = 3.0
         avatarImage.layer.borderColor = UIColor.lightGray.cgColor
-        //addNewCommit
-
-        
-        //testCommit2
-        
-        //testCommit3
-
-        //addNewCommit
-
 
     }
 
